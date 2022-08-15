f25ecfae7efa0bf56eb741c3d6ada7d37ad83770
Mukul K <kumarmuk@gmail.com>
2022-08-15 13:03:30 +0100

    Added a Spring Boot Web project contructs

---
+++ src/main/java/com/nyota/jobcards/JobcardsApplication.java
@@ -0,0 +1,13 @@
+package com.nyota.jobcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobcardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nyota/jobcards/JobcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyota.jobcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
